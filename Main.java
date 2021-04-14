8e9e3ea40d15a2a4e8ee8b2cbb8dd02adc522344
samurollie <53665491+samurollie@users.noreply.github.com>
2021-04-14 04:27:28 -0300

    Organizando o código

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         Employee employeeList[] = new Employee[500];
         int id;
 
-        for(;;) {
+        for (;;) {
             System.out.println("Selecione uma opção:");
             System.out.println("(1) - Adicionar Empregado");
             System.out.println("(2) - Remover Empregado");
@@ -26,105 +26,63 @@ public class Main {
             int cmd = input.nextInt();
             input.nextLine();
 
-            switch(cmd){
-                case 1:
-                    System.out.println("Insira o nome do empregado:");
-                    String name = input.nextLine();
-
-                    System.out.println("Insira o endereco do empregado:");
-                    String address = input.nextLine();
-
-                    System.out.println("Como " + name + " deseja receber o seu salário?");
-                    System.out.println("(1) - Em mãos");
-                    System.out.println("(2) - Depósito bancário");
-                    System.out.println("(3) - Cheque pelos correios");
-                    int payment = input.nextInt();
-                    
-                    System.out.println("Que tipo de empregado " + name + " será?");
-                    System.out.println("(1) - Horista");
-                    System.out.println("(2) - Assalariado");
-                    int type = input.nextInt();
-                    int comissioned = 2;
-                    if (type == 2) {
-                        System.out.println("Será um empregado comissionado?");
-                        System.out.println("(1) - Sim");
-                        System.out.println("(2) - Não");
-                        comissioned = input.nextInt();
-                    }
-
-                    System.out.println("Gerando o nº do cartão...");
-                    id = randInt.nextInt(500); // Gera um número aletório de 0 a 500
-                    while(employeeList[id] != null) { // Null = não tem alguem com aql numero
-                        id = randInt.nextInt(500);
-                    }
-                    
-                    System.out.println("Adicionando empregado " + id);
-                    
-                    if(type == 1) {
-                        employeeList[id] = new Hourly(name, address, id, payment);
-                    } else {
-                        if(comissioned == 1) {
-                            employeeList[id] = new Comissioned(name, address, id, payment, 1, 1, null);
-                        } else {
-                            employeeList[id] = new Salaried(name, address, id, payment, 1);
-                        }
-                    }
-
-                    System.out.println("Empregado adicionado!\n");
-                    System.out.println(employeeList[id].showEmployeeInfo());
-                    break;
-                case 2:
-                    /* System.out.println("Insira o nº do empregado que deseja remover...");
-                    id = input.nextInt();
-                    System.out.println("Removendo empregado " + id);
-                    if (empregados[id] == true) {
-                        empregados[id] = false;
-                        System.out.println("Empregado removido!\n");
-                        break;
-                    }
-                    System.out.println("Empregado não encontrado!\n"); */
-                    break;
-                case 3:
-                    id = randInt.nextInt(500);
-                    System.out.println("Resgistrando ponto para o empregado " + id);
-                    System.out.println("Ponto registrado!\n");
-                    break;
-                case 4:
-                    System.out.println("Resultado de vendas: Fizemos muitas vendas esse mês :D\n");
-                    break;
-                case 5:
-                    System.out.println("Taxa de serviço: 50%\n");
-                    break;
-                case 6:
-                    id = randInt.nextInt(500);
-                    System.out.println("Alterando dados do empregado " + id);
-                    System.out.println("Dados alterados! \n");
-                    break;
-                case 7:
-                    System.out.println("Gerando folha de pagamentos...\n");
-                    System.out.println("Folha de pagamentos gerada!\n");
-                    break;
-                case 8:
-                    System.out.println("Desfazendo ultima ação...\n");
-                    break;
-                case 9:
-                    id = randInt.nextInt(500);
-                    System.out.println("Agenda de pagamentos:\n");
-                    System.out.println("Empregado " + id + ": 30/02\n");
-                    break;
-                case 10:
-                    id = randInt.nextInt(500);
-                    System.out.println("Criando novas agendas...\n");
-                    System.out.println("Empregado " + id + ": 31/02\n");
-                    break;
-                case 11:
-                    System.out.println("Saindo...\n");
-                    input.close();
-                    return;
-                default:
-                    System.out.println("COMANDO INVALIDO\n");
-                    break;
+            switch (cmd) {
+            case 1:
+                Employee.add(employeeList);
+                break;
+            case 2:
+                /*
+                 * System.out.println("Insira o nº do empregado que deseja remover..."); id =
+                 * input.nextInt(); System.out.println("Removendo empregado " + id); if
+                 * (empregados[id] == true) { empregados[id] = false;
+                 * System.out.println("Empregado removido!\n"); break; }
+                 * System.out.println("Empregado não encontrado!\n");
+                 */
+                break;
+            case 3:
+                id = randInt.nextInt(500);
+                System.out.println("Resgistrando ponto para o empregado " + id);
+                System.out.println("Ponto registrado!\n");
+                break;
+            case 4:
+                System.out.println("Resultado de vendas: Fizemos muitas vendas esse mês :D\n");
+                break;
+            case 5:
+                System.out.println("Taxa de serviço: 50%\n");
+                break;
+            case 6:
+                id = randInt.nextInt(500);
+                System.out.println("Alterando dados do empregado " + id);
+                System.out.println("Dados alterados! \n");
+                break;
+            case 7:
+                System.out.println("Gerando folha de pagamentos...\n");
+                System.out.println("Folha de pagamentos gerada!\n");
+                break;
+            case 8:
+                System.out.println("Desfazendo ultima ação...\n");
+                break;
+            case 9:
+                id = randInt.nextInt(500);
+                System.out.println("Agenda de pagamentos:\n");
+                System.out.println("Empregado " + id + ": 30/02\n");
+                break;
+            case 10:
+                id = randInt.nextInt(500);
+                System.out.println("Criando novas agendas...\n");
+                System.out.println("Empregado " + id + ": 31/02\n");
+                break;
+            case 11:
+                System.out.println("Saindo...\n");
+                input.close();
+                return;
+            default:
+                System.out.println("COMANDO INVALIDO\n");
+                break;
             }
+
+            System.out.println("Pressione qualquer tecla para continuar...");
+            input.nextLine();
         }
 
     }
--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,18 +1,19 @@
 package src.employee;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public abstract class Employee {
     protected String name;
     protected String adress;
     protected int card;
     protected String paymentMethod;
-    public static int quantity = 0;
 
     public Employee(String name, String adress, int card, int paymentMethod) {
         this.name = name;
         this.adress = adress;
         this.card = card;
         this.setPaymentMethod(paymentMethod);
-        quantity++;
     }
 
     public abstract String showEmployeeInfo();
@@ -28,5 +29,56 @@ public abstract class Employee {
             System.out.println("Metodo de pagamento inválido!\n");
         }
     }
+
+    public static void add(Employee employeeList[]) {
+        Random randInt = new Random();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o nome do empregado:");
+        String name = input.nextLine();
+
+        System.out.println("Insira o endereco do empregado:");
+        String address = input.nextLine();
+
+        System.out.println("Como " + name + " deseja receber o seu salário?");
+        System.out.println("(1) - Em mãos");
+        System.out.println("(2) - Depósito bancário");
+        System.out.println("(3) - Cheque pelos correios");
+        int payment = input.nextInt();
+
+        System.out.println("Que tipo de empregado " + name + " será?");
+        System.out.println("(1) - Horista");
+        System.out.println("(2) - Assalariado");
+        int type = input.nextInt();
+        int comissioned = 2;
+        if (type == 2) {
+            System.out.println("Será um empregado comissionado?");
+            System.out.println("(1) - Sim");
+            System.out.println("(2) - Não");
+            comissioned = input.nextInt();
+        }
+
+        System.out.println("Gerando o nº do cartão...");
+        int id = randInt.nextInt(500); // Gera um número aletório de 0 a 500
+        while (employeeList[id] != null) { // Null = não tem alguem com aql numero
+            id = randInt.nextInt(500);
+        }
+
+        System.out.println("Adicionando empregado " + id);
+
+        if (type == 1) {
+            employeeList[id] = new Hourly(name, address, id, payment);
+        } else {
+            if (comissioned == 1) {
+                employeeList[id] = new Comissioned(name, address, id, payment, 1, 1, null);
+            } else {
+                employeeList[id] = new Salaried(name, address, id, payment, 1);
+            }
+        }
+
+        System.out.println("Empregado adicionado!\n");
+        System.out.println(employeeList[id].showEmployeeInfo());
+    }
+
 
 }
