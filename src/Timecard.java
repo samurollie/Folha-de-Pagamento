7ded889927f256b1201b229e65b0c64406ee84d5
samurollie <53665491+samurollie@users.noreply.github.com>
2021-04-21 04:50:34 -0300

    Finalizando Horista

---
+++ src/Timecard.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Timecard {
+    public int workedHours;
+    public String day;
+
+    public Timecard(int workedHours, String day) {
+        this.workedHours = workedHours;
+        this.day = day;
+    }
+}
--- src/employee/Hourly.java
+++ src/employee/Hourly.java
@@ -1,31 +1,39 @@
 package src.employee;
 
+import java.util.ArrayList;
+
+import src.Timecard;
+
 public class Hourly extends Employee{
-    private double workedHours;
-    private double serviceCharge;
+    private ArrayList<Timecard> timeCards = new ArrayList<Timecard>();
 
     public Hourly(String name, String adress, int card, int paymentMethod) {
         super(name, adress, card, paymentMethod);
     }
 
-    public double getServiceCharge() {
-        return serviceCharge;
+    public void getTimeCards() {
+        int total = 0;
+        for (Timecard timecard : timeCards) {
+            System.out.println("Dia: " + timecard.day);
+            System.out.println("Horas Trabahadas: " + timecard.workedHours);
+            System.out.println("#############\n");
+            total += timecard.workedHours;
+        }
+
+        System.out.println("Total de horas trabalhadas: " + total);
     }
 
-    public void setServiceCharge(double serviceCharge) {
-        this.serviceCharge = serviceCharge;
+    public void setTimeCards(int workedHours, String day) {
+        Timecard timeCard = new Timecard(workedHours, day);
+        this.timeCards.add(timeCard);
     }
 
-    public double getWorkedHours() {
-        return workedHours;
-    }
-
-    public void setWorkedHours(double workedHours) {
-        this.workedHours = workedHours;
-    }
-
-    @Override
     public String showEmployeeInfo() {
         return "Nome: " + this.name + "\nEndereco: " + this.adress +"\nNumero do cartao: " + this.card + "\nMetodo de Pagamento: " + this.paymentMethod;
     }
+
+    /* @Override
+    public Hourly add() {
+        return ;
+    } */
 }
