e5f408ad1e6ce20f0fdc189ff874a036054b5772
samurollie <53665491+samurollie@users.noreply.github.com>
2021-04-24 23:06:11 -0300

    Sell -> Sales

--- src/Sale.java
+++ src/Sale.java
@@ -1,12 +1,12 @@
 package src;
 
-public class Sell {
+public class Sale {
     int date;
     double value;
     String item;
     int employeeId;
     
-    public Sell(int date, double value, String item, int employeeId) {
+    public Sale(int date, double value, String item, int employeeId) {
         this.date = date;
         this.value = value;
         this.item = item;
